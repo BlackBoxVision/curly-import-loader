fcd10f0f1bdc95b761bd73db991cbeb2bacaad98
Daniel Gómez <dani91.gomez@gmail.com>
2016-07-27 01:33:48 -0300

    Added parser and babel jsx conversion to es6

--- src/curlyLoader.js
+++ src/curlyLoader.js
@@ -1,5 +1,17 @@
+import { parseModule } from 'shift-parser';
+import { transform }  from 'babel-core';
+import colors from 'colors'
+
 const optimizeCurlyImports = (file, callback) => {
-    console.log(file);
+
+    let toEs6 = transform(file, {presets: "react"}).code;
+    let parsedData = parseModule(toEs6);
+    let importedModules = parsedData.items.find(item => item.type === "Import");
+
+    console.log("Processing code :\n".grey.underline + toEs6);
+    console.log("Parser result: \n".grey.underline + JSON.stringify(parsedData));
+    console.log("Imports to be processed : \n".grey.underline + JSON.stringify(importedModules));
+
     return callback(null, file);
 }
 
